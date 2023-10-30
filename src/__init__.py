8f877c79bae07a9d22f198594487cbedad763b19
Münker, Simon <muenker@uni-trier.de>
2023-10-30 17:57:33 +0100

    allow for selected cors

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from starlette.middleware.cors import CORSMiddleware
 
 from .routers import create_explicit_agent_route
 
@@ -13,6 +14,17 @@ def create_app(config: dict):
         title=config["TITLE"],
         description=open(f'{config["DOCS"]}/Root.md').read(),
         version=config["VERSION"],
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            "http://localhost:5173",
+            "https://replyrift.xciv.de"
+        ],
+        allow_credentials=False,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     @app.get("/")
