24b770b1155eabce3023fe21d35771621612a9df
Simon Münker <simon.muenker@googlemail.com>
2025-01-21 08:56:27 +0000

    explictly disable sampling

--- src/twon_agents/util.py
+++ src/twon_agents/util.py
@@ -6,11 +6,15 @@ from rich.progress import track
 import transformers
 
 
-def load_pipelines(models: typing.Dict[str, str]) -> typing.Dict[str, transformers.Pipeline]:
+def load_pipelines(models: typing.Dict[str, str], disable_sampling: bool = True) -> typing.Dict[str, transformers.Pipeline]:
     pipelines: typing.Dict[str, transformers.Pipeline] = {
         label: transformers.pipeline("text-generation", slug)
         for n, (label, slug) in enumerate(models.items())
     }
+
+    if disable_sampling:
+        for pipeline in pipelines.values():
+            pipeline.model.generation_config.do_sample = False
 
     if "adapter" in pipelines.keys():
         pipelines["adapter"].model.load_adapter(models["adapter"])
